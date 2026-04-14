5aaa9d4b21fa2d3cca8ceb2623727b89704854ec
Giancarlo Sanchez <gianksp@gmail.com>
2026-04-14 13:05:46 +0100

    Logo improvements

--- src/components/ScriptureGraphLogo.jsx
+++ src/components/ScriptureGraphLogo.jsx
@@ -1,7 +1,7 @@
 export default function ScriptureGraphLogo({ size = 64 }) {
     return (
         <img
-            src="/icons/logo-transparent.svg"
+            src="/icons/logo.svg"
             width={size}
             height={size}
             alt="Scripture Graph"
